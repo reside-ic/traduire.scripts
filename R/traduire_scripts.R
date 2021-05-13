edde52d1329c7225cf10bffe7c5b8f43e0533569
Robert Ashton <r.ashton@imperial.ac.uk>
2021-05-13 16:09:54 +0100

    Add initial script and package setup

---
+++ R/traduire_scripts.R
@@ -0,0 +1,26 @@
+make_translation_summary <- function(output_path, ...) {
+  browser()
+  args <- list(...)
+  translations <- lapply(args, summarise_dir)
+  ## / is special character and cannot be used as name of a sheet in xl
+  names(translations) <- gsub("/", "-", args)
+  message(sprintf("Writing output to %s", output_path))
+  writexl::write_xlsx(translations, output_path)
+}
+
+summarise_dir <- function(path) {
+  json <- list.files(path, pattern = "*.json", full.names = TRUE)
+  translations <- lapply(json, function(file) {
+    lang <- strsplit(basename(file), "-")[[1]][1]
+    content <- jsonlite::read_json(file)
+    content <- data.frame(key = names(content),
+                          value = unname(unlist(content)))
+    names(content) <- c("key", lang)
+    content
+  })
+  merge.all <- function(x, y) {
+    merge(x, y, all = TRUE, by = "key")
+  }
+
+  Reduce(merge.all, translations)
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# traduire-scripts
+
+Contains R script which might help when collating strings when asking for translations
+
+TODO: Allow adding a date since - to only show new strings since that date
